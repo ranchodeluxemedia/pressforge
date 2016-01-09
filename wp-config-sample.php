88c87df20f1975e6b2349759b211f7f72d062b89
Chris Canterbury <chris@ranchodeluxemedia.com>
2016-01-09 04:57:41 -0600

    config stuff

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'forge');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'forge');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'Wwmb8arNRMsNj4wjnM2s');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
