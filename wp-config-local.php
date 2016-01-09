553f24ceabdbedf53232eb17647e0c3790dec9c1
Chris Canterbury <chris@ranchodeluxemedia.com>
2016-01-09 04:37:23 -0600

    initialize repository

--- wp-config-local.php
+++ wp-config-local.php
@@ -1,13 +1,13 @@
 <?php
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'ranchopress');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'secret');
+define('DB_PASSWORD', '@31169Ccc!');
 define('DB_HOST', 'localhost');
 define('WP_DEBUG', true);
 define('DB_CHARSET', 'utf8');
 
-define('WP_SITEURL', 'http://wordpress.dev:8000/');
-define('WP_HOME', 'http://wordpress.dev:8000/');
+define('WP_SITEURL', 'http://ranchodeluxemedia.dev/');
+define('WP_HOME', 'http://ranchodeluxemedia.dev/');
 define( 'JETPACK_DEV_DEBUG', true);
 $table_prefix  = 'wp_';
 define('WPLANG', '');
